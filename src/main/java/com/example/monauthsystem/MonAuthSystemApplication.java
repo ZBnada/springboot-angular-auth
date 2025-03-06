458ce1cbb73b72610237e8d937963ad8bdabd31b
ZBnada <nadazouabi5@gmail.com>
2025-03-07 00:51:13 +0100

    Acommit initial

---
+++ src/main/java/com/example/monauthsystem/MonAuthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.monauthsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonAuthSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonAuthSystemApplication.class, args);
+    }
+
+}
